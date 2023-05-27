ba98d17eaf827971e9651f73832f620f15e30ed3
fernandesd <diegofernandes.dev@gmail.com>
2023-05-27 02:38:56 -0300

    [product] core: cria trait para centralizar a estrutura do retorno da controller

--- product/app/Http/Controllers/Controller.php
+++ product/app/Http/Controllers/Controller.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Traits\JsonResponser;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+    use JsonResponser;
     //
 }
