48164df607dcbee2952a0dac52ca7ad1d55cbb77
WellitonCunha <wellitonsousa@live.com>
2023-05-28 00:22:47 -0300

    feat: criando a rota de intenção de compra

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\v1\BuyIntentionController;
 use App\Http\Controllers\api\v1\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,6 +24,12 @@ Route::prefix('v1')->group(function (){
     Route::prefix('product')->group(function(){
         Route::get('/index', [ProductController::class, 'index'])->name('api.v1.product.index');
         Route::get('/product_edit/{id}', [ProductController::class, 'productEdit'])->name('api.v1.product.product_edit');
+        Route::post('/product_view', [ProductController::class, 'productView'])->name('api.v1.product.product_view');
+    });
+
+    Route::prefix('buy_intention')->group(function(){
+        Route::get('/index', [BuyIntentionController::class, 'index'])->name('api.v1.buy_intention.index');
+        Route::post('/store', [BuyIntentionController::class, 'store'])->name('api.v1.buy_intention.store');
     });
 });
 
