8b6c9766ca8c88fca528bfe16f6f1e231f669add
WellitonCunha <wellitonsousa@live.com>
2023-05-28 00:18:59 -0300

    feat: criado a listagem de intenção de compra com os produtos selecionados

--- app/Repository/BuyIntentionRepository.php
+++ app/Repository/BuyIntentionRepository.php
@@ -3,11 +3,24 @@
 namespace App\Repository;
 
 use App\Models\BuyIntention;
+use Illuminate\Database\Eloquent\Model;
+use PHPUnit\TestRunner\TestResult\Collector;
 
 class BuyIntentionRepository extends RepositoryBase
 {
-    public function __construct(BuyIntention $model)
+    protected $productRepository;
+    public function __construct(BuyIntention $model, ProductRepository $productRepository)
     {
         parent::__construct($model);
+        $this->productRepository = $productRepository;
+    }
+
+    public function index()
+    {
+        $result = $this->model->get();
+        foreach ($result as $key => $value) {
+            $value->product_id = $this->productRepository->productView(json_decode($value->product_id));
+        }
+        return $result;
     }
 }
