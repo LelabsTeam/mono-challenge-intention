9e6ab8e54505323a58d222a0735822c2557f9c92
fernandesd <diegofernandes.dev@gmail.com>
2023-05-29 00:29:01 -0300

    [product] routes: redireciona o index para a lista de produtos

--- product/src/app.controller.ts
+++ product/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AxiosResponse } from 'axios';
 import { Observable } from 'rxjs';
@@ -7,7 +7,9 @@ import { Observable } from 'rxjs';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  
+  @Get()
+  @Redirect(process.env.PRODUCTS_URL, 301)
+
   @Get('ping')
   ping(): {} {
     const response = this.appService.ping();
